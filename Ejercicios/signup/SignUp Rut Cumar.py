821458650d0ce99886490ba1cae469f4c43d6fe6
Abixs-ch <abiluvxs05@gmail.com>
2026-04-30 12:59:08 -0600

    SignUp Rut Cumar.py

--- Ejercicios/signup/SignUp Rut Cumar.py
+++ Ejercicios/signup/SignUp Rut Cumar.py
@@ -1,10 +1,40 @@
 from customtkinter import *
-set_appearance_mode("dark")
-set_default_color_theme("dark-blue")
 
+def click_boton_aceptar():
+    nombre=campo_nombre.get().strip()
+    apellido=campo_apellido.get().strip()
+    correo=campo_correo.get().strip()
+    edad=campo_edad.get().strip()
+    telefono=campo_telefono.get().strip()
+    contraseña=campo_contraseña.get().strip()
+    
+    if (not nombre
+        or not apellido
+        or not correo
+        or not edad
+        or not contraseña
+        or not telefono):
+        print("Por favor rellene todos los campos.")
+    else:
+        if not nombre.isalpha():
+            print("El nombre solo debe contener letras.")
+            return
+        if not apellido.isalpha():
+            print("El apellido solo debe contener letras.")
+            return
+        if telefono.isdigit() or len(telefono)!=8:
+            print("El telefono debe contener exactamente 8 digitos numericos.")
+            return
+        print("Regristro completado con exito.")
+        
+
+set_appearance_mode("light")
+set_default_color_theme("green")
+
+#Ventana------------------------------------------------------------------------------
 ventana = CTk()
 ventana.title("Titulo de la ventana")
-ventana.geometry("600x800")
+ventana.geometry("500x800")
 ventana.grid_columnconfigure(0, weight=1)
 ventana.grid_rowconfigure(0, weight=1)
 
@@ -36,68 +66,58 @@ etiqueta_titulo = CTkLabel(
 etiqueta_titulo.grid(row=0,column=0,)
 
 #Frame Datos------------------------------------------------------------------------------
-
 frame_datos= CTkFrame(master=frame_principal,
                       corner_radius=0)
 frame_datos.grid(row=1, column=0, sticky= "nsew", padx=10, pady=10)
 frame_datos.grid_columnconfigure(0, weight=1)
 frame_datos.grid_columnconfigure(1, weight=1)
-frame_datos.grid_rowconfigure(0, weight=1)
-frame_datos.grid_rowconfigure(1, weight=1)
-frame_datos.grid_rowconfigure(2, weight=1)
-frame_datos.grid_rowconfigure(3, weight=1)
-frame_datos.grid_rowconfigure(4, weight=1)
-frame_datos.grid_rowconfigure(5, weight=1)
+for i in range(6):
+    frame_datos.grid_rowconfigure(i, weight=1)
 
-campo_texto = CTkEntry(
+#Ingresar texto------------------------------------------------------------------------------
+campo_nombre = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su nombre...",
-    font=("Montserrat", 16),
-)
-#Ingresar texto------------------------------------------------------------------------------
-campo_texto.grid(row=0, column=1, sticky="w", pady=15)
-campo_texto = CTkEntry(
+    font=("Montserrat", 16))
+campo_nombre.grid(row=0, column=1, sticky="w", pady=15)
+campo_apellido = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su apellido...",
-    font=("Montserrat", 16),
-)
-campo_texto.grid(row=1, column=1, sticky="w", pady=15)
-campo_texto = CTkEntry(
+    font=("Montserrat", 16))
+campo_apellido.grid(row=1, column=1, sticky="w", pady=15)
+campo_correo = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su correo...",
-    font=("Montserrat", 16),
-)
-campo_texto.grid(row=2, column=1, sticky="w", pady=15)
-campo_texto = CTkEntry(
+    font=("Montserrat", 16))
+campo_correo.grid(row=2, column=1, sticky="w", pady=15)
+campo_telefono = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su telefono...",
-    font=("Montserrat", 16),
-)
-campo_texto.grid(row=3, column=1, sticky="w", pady=15)
-campo_texto = CTkEntry(
+    font=("Montserrat", 16))
+campo_telefono.grid(row=3, column=1, sticky="w", pady=15)
+campo_edad = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su edad...",
-    font=("Montserrat", 16),
-)
-campo_texto.grid(row=4, column=1,sticky="w", pady=15)
-campo_texto = CTkEntry(
+    font=("Montserrat", 16))
+campo_edad.grid(row=4, column=1,sticky="w", pady=15)
+campo_contraseña = CTkEntry(
     master=frame_datos,
     width=180,
     corner_radius=0,
     placeholder_text="Escriba su Contraseña...",
     font=("Montserrat", 16),
-)
-campo_texto.grid(row=5, column=1, sticky="w", pady=15)
+    show="*")
+campo_contraseña.grid(row=5, column=1, sticky="w", pady=15)
 #nombres------------------------------------------------------------------------------
 etiqueta_nom = CTkLabel(
     master=frame_datos,
@@ -161,7 +181,7 @@ boton_1 = CTkButton(master=frame_boton,
                     text="Aceptar",
                     anchor="center",
                     font=("Montserrat", 16),
-                    command=funcion_boton_1)
+                    command=click_boton_aceptar)
 boton_1.grid( row=0, column=0,)
 
 ventana.mainloop()
