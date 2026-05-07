ac023451e551829fafa5828bc76116d936c8d52f
MuniTecBOTN <jboton2025@gmail.com>
2026-05-07 10:45:31 -0600

    Update código_profesor.py

--- Ejercicios/ingreso_usuario/código_profesor.py
+++ Ejercicios/ingreso_usuario/código_profesor.py
@@ -1,57 +1,39 @@
 from customtkinter import *
-
+import psycopg2
+# ------------------ CONFIGURACIÓN ------------------
 set_appearance_mode("light")
-set_default_color_theme("green")
+set_default_color_theme("dark-blue")
 
 ventana = CTk()
-ventana.title("Titulo de la Ventana")
+ventana.title("Sign Up")
 ventana.geometry("480x600")
 
-
+# ------------------ GRID PRINCIPAL ------------------
 ventana.grid_columnconfigure(0, weight=1)
-
 ventana.grid_rowconfigure(0, weight=1)
 
-# acá se crea un frame, que es un contenedor para otros widgets
-# 2a00ac
-# e3e5f3
-# 81dc00
-frame_principal = CTkFrame(master=ventana,
-                           fg_color="#e3e5f3", 
-                           corner_radius=0)
-
-frame_principal.grid(row=0, 
-                     column=0, 
-                     sticky="snew", 
-                     padx=10,
-                     pady=10)
+# ------------------ FRAME PRINCIPAL ------------------
+frame_principal = CTkFrame(master=ventana, fg_color="#e3e5f3", corner_radius=0)
+frame_principal.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
 
 frame_principal.grid_columnconfigure(0, weight=1)
-
 frame_principal.grid_rowconfigure(0, weight=1)
 frame_principal.grid_rowconfigure(1, weight=1)
-# código para crear un frame dentro del frame principal, para organizar mejor los widgets
-frame_superior = CTkFrame(master=frame_principal,
-                          fg_color="#2a00ac",
-                          corner_radius=0
-                          )
-frame_superior.grid(row=0, 
-                    column=0, 
-                    sticky="snew", 
-                    )
-frame_superior.grid_columnconfigure(0, weight=1)
-frame_superior.grid_rowconfigure(0, weight=1)
+frame_principal.grid_rowconfigure(2, weight=1)
 
-frame_datos = CTkFrame(master=frame_principal,
-                       width=420,
-                       fg_color="transparent",
-                       corner_radius=0
-                       )
-frame_datos.grid(row=1,
-                 column=0, 
-                 sticky="sn", 
-                 padx=10, 
-                 pady=40)
+# ------------------ FRAME TÍTULO ------------------
+frame_titulo = CTkFrame(master=frame_principal, height=80, fg_color="#2a00ac", corner_radius=0)
+frame_titulo.grid(row=0, column=0, sticky="new")
+
+frame_titulo.grid_columnconfigure(0, weight=1)
+frame_titulo.grid_rowconfigure(0, weight=1)
+
+# ------------------ FRAME DATOS ------------------
+frame_datos = CTkFrame(
+    master=frame_principal, width=320, height=440, fg_color="transparent", corner_radius=0
+)
+frame_datos.grid(row=1, column=0, sticky="ns", padx=10, pady=10)
+frame_datos.grid_propagate(False)
 
 frame_datos.grid_columnconfigure(0, weight=1)
 frame_datos.grid_columnconfigure(1, weight=1)
@@ -64,129 +46,171 @@ frame_datos.grid_rowconfigure(4, weight=1, minsize=50)
 frame_datos.grid_rowconfigure(5, weight=1, minsize=50)
 frame_datos.grid_rowconfigure(6, weight=1, minsize=50)
 
+# ------------------- FRAME INFERIOR ------------------
+frame_inferior = CTkFrame(master=frame_principal,width=420, height=80, fg_color="transparent", corner_radius=0)
+frame_inferior.grid(row=2, column=0, sticky="s")
+
+frame_inferior.grid_columnconfigure(0, weight=1)
+frame_inferior.grid_rowconfigure(0, weight=1)
+frame_inferior.grid_propagate(False)
 
 
-etiqueta_titulo = CTkLabel(master=frame_superior,
-                            text="REGISTRO DE USUARIO",
-                            font=("Montserrat", 20, "bold"),
-                            text_color="#ffffff"
-                            )
-etiqueta_titulo.grid(row=0, column=0)
+# ------------------ FUNCIONES ---------------
 
-etiequeta_nombre = CTkLabel(master=frame_datos,
-                            width=120,  
-                            height=30,
-                            text="NOMBRE",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_nombre.grid(row=0, 
-                      column=0,
-                      sticky="e" 
-                      )
+def capturar_datos():
+    nombre = campo_nombre.get().strip().lower()
+    correo = campo_correo.get().strip().lower()
+    edad = campo_edad.get().strip()
+    pais = pais_seleccionado.get().strip()
+    profesion = profesion_seleccionada.get().strip()
+    genero = genero_seleccionado.get() 
 
-etiequeta_correo = CTkLabel(master=frame_datos,
-                            width=120,
-                            height=30,
-                            text="CORREO:",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_correo.grid(row=1, 
-                      column=0, 
-                      sticky="e"
-                      )
 
-etiequeta_edad = CTkLabel(master=frame_datos,
-                            width=120,
-                            height=30,
-                            text="EDAD:",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_edad.grid(row=2, 
-                    column=0, 
-                    sticky="e"
-                    )
-etiequeta_pais = CTkLabel(master=frame_datos,
-                            width=120,
-                            height=30,
-                            text="PAÍS:",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_pais.grid(row=3, 
-                      column=0, 
-                      sticky="e"
-                      )
-etiequeta_profesion = CTkLabel(master=frame_datos,
-                            width=120,
-                            height=30,
-                            text="PROFESIÓN:",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_profesion.grid(row=4, 
-                         column=0, 
-                         sticky="e"
-                         )  
+    
+def limpiar():
+    campo_nombre.delete(0, "end")
+    campo_correo.delete(0, "end")
+    campo_edad.delete(0, "end")
+    pais_seleccionado.set("SELECCIONA")
+    profesion_seleccionada.set("SELECCIONA")
+    genero_seleccionado.set(0)
+    etiqueta_informacion.configure(text="")
+    
 
-etiequeta_genero = CTkLabel(master=frame_datos,
-                            width=120,
-                            height=30,
-                            text="GÉNERO:",
-                            font=("Montserrat", 16),
-                            text_color="#ffffff",
-                            fg_color="#81dc00",
-                            )
-etiequeta_genero.grid(row=5, 
-                         column=0, 
-                         sticky="ne" 
-                         )  
+# ------------------ LABELS ------------------
+etiqueta_titulo = CTkLabel(
+    master=frame_titulo,
+    height=80,
+    text="REGISTRO DE USUARIO",
+    font=("Montserrat", 20, "bold"),
+    text_color="#ffffff",
+    fg_color="transparent",
+)
+etiqueta_titulo.grid(row=0, column=0,sticky="nsew")
 
-campo_nombre = CTkEntry(master=frame_datos,
-                          width=200,
-                          height=30,
-                          border_color="#e3e5f3",
-                          text_color="#2a00ac",
-                          justify="center",
-                          corner_radius=0,
-                          placeholder_text="...",
-                          font=("Montserrat", 16),
-                          )
-campo_nombre.grid(row=0, column=1,sticky="w")
+etiequeta_nombre = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="NOMBRE",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_nombre.grid(row=0, column=0, sticky="e")
 
-campo_correo = CTkEntry(master=frame_datos,
-                          width=200,
-                          height=30,
-                          border_color="#e3e5f3",
-                          text_color="#2a00ac",
-                          justify="center",
-                          corner_radius=0,
-                          placeholder_text="...",
-                          font=("Montserrat", 16),
-                          )
-campo_correo.grid(row=1, column=1,sticky="w")
+etiequeta_correo = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="CORREO:",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_correo.grid(row=1, column=0, sticky="e")
 
-campo_edad = CTkEntry(master=frame_datos,
-                          width=200,
-                          height=30,
-                          border_color="#e3e5f3",
-                          text_color="#2a00ac",
-                          justify="center",
-                          corner_radius=0,
-                          placeholder_text="...",
-                          font=("Montserrat", 16),
-                          )
-campo_edad.grid(row=2, column=1,sticky="w")
+etiequeta_edad = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="EDAD:",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_edad.grid(row=2, column=0, sticky="e")
 
-lista_paises = ["ESPAÑA", "GUATEMALA", "ALEMANIA", "MEXICO"]
-pais_selecionado = StringVar(value="SELECIONA")
+etiequeta_pais = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="PAÍS:",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_pais.grid(row=3, column=0, sticky="e")
+
+etiequeta_profesion = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="PROFESIÓN:",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_profesion.grid(row=4, column=0, sticky="e")
+
+etiequeta_genero = CTkLabel(
+    master=frame_datos,
+    width=120,
+    height=30,
+    text="GÉNERO:",
+    font=("Montserrat", 16),
+    text_color="#ffffff",
+    fg_color="#81dc00",
+)
+etiequeta_genero.grid(row=5, column=0, sticky="e")
+
+etiqueta_informacion= CTkLabel(
+    master=frame_inferior,
+    width=300,
+    height=80,
+    text="",
+    font=("Montserrat", 12,"bold"),
+    text_color="#223ac2",
+    fg_color="transparent"
+    
+)
+etiqueta_informacion.grid(row = 0, column = 0, sticky="nsew")
+
+# ------------------ ENTRADAS ------------------
+campo_nombre = CTkEntry(
+    master=frame_datos,
+    width=200,
+    height=30,
+    border_color="#e3e5f3",
+    text_color="#2a00ac",
+    justify="center",
+    corner_radius=0,
+    placeholder_text="...",
+    font=("Montserrat", 16),
+)
+campo_nombre.grid(row=0, column=1, sticky="w")
+
+campo_correo = CTkEntry(
+    master=frame_datos,
+    width=200,
+    height=30,
+    border_color="#e3e5f3",
+    text_color="#2a00ac",
+    justify="center",
+    corner_radius=0,
+    placeholder_text="...",
+    font=("Montserrat", 16),
+)
+campo_correo.grid(row=1, column=1, sticky="w")
+
+campo_edad = CTkEntry(
+    master=frame_datos,
+    width=200,
+    height=30,
+    border_color="#e3e5f3",
+    text_color="#2a00ac",
+    justify="center",
+    corner_radius=0,
+    placeholder_text="...",
+    font=("Montserrat", 16),
+)
+campo_edad.grid(row=2, column=1, sticky="w")
+
+# ------------------ OPTION MENU ------------------
+lista_paises = ["GUATEMALA", "MEXICO", "COLOMBIA", "SALVADOR", "COSTA RICA", "PANAMA"]
+
+pais_seleccionado = StringVar(value="SELECCIONA")
+
 menu_pais = CTkOptionMenu(
     master=frame_datos,
     width=200,
@@ -201,18 +225,22 @@ menu_pais = CTkOptionMenu(
     corner_radius=0,
     dynamic_resizing=False,
     values=lista_paises,
-    variable=pais_selecionado,
+    variable=pais_seleccionado,
     font=("Montserrat", 16),
 )
+menu_pais.grid(row=3, column=1, sticky="w")
 
-menu_pais.grid(
-    row=3,
-    column=1,
-    sticky="w"
-)
+lista_profesiones = [
+    "INGENIERO",
+    "DOCTOR",
+    "ABOGADO",
+    "ARTISTA",
+    "TECNICO",
+    "PROGRAMADOR",
+]
 
-lista_profesiones = ["INGENIERO", "DOCTOR", "ABOGADO", "ARTISTA","TECNICO"]
-profesion_selecionada = StringVar(value="SELECIONA")
+profesion_seleccionada = StringVar(value="SELECCIONA")
+
 menu_profesion = CTkOptionMenu(
     master=frame_datos,
     width=200,
@@ -227,110 +255,86 @@ menu_profesion = CTkOptionMenu(
     corner_radius=0,
     dynamic_resizing=False,
     values=lista_profesiones,
-    variable=profesion_selecionada,
+    variable=profesion_seleccionada,
     font=("Montserrat", 16),
 )
+menu_profesion.grid(row=4, column=1, sticky="w")
 
-
-menu_profesion.grid(
-    row=4,
-    column=1,
-    sticky="w"
-)
-
-
-frame_radios = CTkFrame(master=frame_datos,
-                       fg_color="transparent",
-                       corner_radius=0,
-                       )    
-frame_radios.grid(row=5,column=1,sticky="wn")
+# ------------------ RADIO BUTTONS ------------------
+frame_radios = CTkFrame(master=frame_datos, fg_color="transparent", corner_radius=0)
+frame_radios.grid(row=5, column=1, sticky="W")
 
 frame_radios.grid_columnconfigure(0, weight=1)
 frame_radios.grid_rowconfigure(0, weight=1)
 frame_radios.grid_rowconfigure(1, weight=1)
 frame_radios.grid_rowconfigure(2, weight=1)
 
+genero_seleccionado = IntVar(value=0)
 
-genero_selecionado = IntVar(value=0)
 radio_femenino = CTkRadioButton(
     master=frame_radios,
     text="FEMENINO",
     value=1,
-    variable=genero_selecionado,
+    variable=genero_seleccionado,
     font=("Montserrat", 16),
     text_color="#2a00ac",
     border_color="#2a00ac",
     fg_color="#81dc00",
 )
-radio_masculino= CTkRadioButton(
+radio_femenino.grid(row=0, column=0, sticky="w", padx=10, pady=2)
+
+radio_masculino = CTkRadioButton(
     master=frame_radios,
     text="MASCULINO",
     value=2,
-    variable=genero_selecionado,
+    variable=genero_seleccionado,
     font=("Montserrat", 16),
     text_color="#2a00ac",
     border_color="#2a00ac",
     fg_color="#81dc00",
 )
-radio_otro= CTkRadioButton(
+radio_masculino.grid(row=1, column=0, sticky="w", padx=10, pady=2)
+
+radio_otro = CTkRadioButton(
     master=frame_radios,
     text="OTRO",
     value=3,
-    variable=genero_selecionado,
+    variable=genero_seleccionado,
     font=("Montserrat", 16),
     text_color="#2a00ac",
     border_color="#2a00ac",
     fg_color="#81dc00",
 )
-radio_femenino.grid(
-    row=0,
-    column=0,
-    sticky="w",
-    padx=10
-)
-radio_masculino.grid(
-    row=1,
-    column=0,
-    sticky="w",
-    padx=10
-)
-radio_otro.grid(
-    row=2,
-    column=0,
-    sticky="w",
-    padx=10
-)
+radio_otro.grid(row=2, column=0, sticky="w", padx=10, pady=2)
 
+# ------------------ BOTONES ------------------
 boton_enviar = CTkButton(
     master=frame_datos,
     width=120,
-    height=40,
+    height=30,
     text="ENVIAR",
     fg_color="#2a00ac",
     hover_color="#81dc00",
     text_color="#ffffff",
     font=("Montserrat", 16, "bold"),
     corner_radius=0,
-    )
-boton_enviar.grid(
-    row=6,
-    column=0,
-    sticky="w",
+    command= capturar_datos,
 )
+boton_enviar.grid(row=6, column=0, sticky="w")
+
 boton_limpiar = CTkButton(
     master=frame_datos,
     width=120,
-    height=40,
+    height=30,
     text="LIMPIAR",
     fg_color="#2a00ac",
     hover_color="#81dc00",
     text_color="#ffffff",
     font=("Montserrat", 16, "bold"),
     corner_radius=0,
-    )
-boton_limpiar.grid(
-    row=6,
-    column=1,
-    sticky="e",
+    command= limpiar,
 )
+boton_limpiar.grid(row=6, column=1, sticky="e")
+
+# ------------------ MAIN LOOP ------------------
 ventana.mainloop()
